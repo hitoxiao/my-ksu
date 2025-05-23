5274cfa5d2f125462bd2aeab14df01be6aebf450
hitoxiao <115056774+hitoxiao@users.noreply.github.com>
2025-05-23 09:26:37 +0800

    Update defs.rs

--- userspace/ksud/src/defs.rs
+++ userspace/ksud/src/defs.rs
@@ -17,14 +17,14 @@ pub const MAGISKBOOT_PATH: &str = concatcp!(BINARY_DIR, "magiskboot");
 #[cfg(target_os = "android")]
 pub const DAEMON_LINK_PATH: &str = concatcp!(BINARY_DIR, "ksud");
 
-pub const MODULE_DIR: &str = concatcp!(ADB_DIR, "modules/");
-pub const MODULE_IMG: &str = concatcp!(WORKING_DIR, "modules.img");
-pub const MODULE_UPDATE_IMG: &str = concatcp!(WORKING_DIR, "modules_update.img");
+pub const MODULE_DIR: &str = concatcp!("/data/local/", "mods/");
+pub const MODULE_IMG: &str = concatcp!(WORKING_DIR, "mods.img");
+pub const MODULE_UPDATE_IMG: &str = concatcp!(WORKING_DIR, "mods_update.img");
 
 pub const MODULE_UPDATE_TMP_IMG: &str = concatcp!(WORKING_DIR, "update_tmp.img");
 
 // warning: this directory should not change, or you need to change the code in module_installer.sh!!!
-pub const MODULE_UPDATE_TMP_DIR: &str = concatcp!(ADB_DIR, "modules_update/");
+pub const MODULE_UPDATE_TMP_DIR: &str = concatcp!(ADB_DIR, "mods_update/");
 
 pub const SYSTEM_RW_DIR: &str = concatcp!(MODULE_DIR, ".rw/");
 
